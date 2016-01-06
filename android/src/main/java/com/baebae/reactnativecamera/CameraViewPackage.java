34a8e02f81bccd576c157a116978a72bfdeb5239
Andrew Crowell <andrewj.crowell@gmail.com>
2016-01-05 16:24:30 -0800

    Add import for instance manager

--- android/src/main/java/com/baebae/reactnativecamera/CameraViewPackage.java
+++ android/src/main/java/com/baebae/reactnativecamera/CameraViewPackage.java
@@ -6,6 +6,8 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import com.baebae.reactnativecamera.cameralib.helpers.CameraInstanceManager;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
